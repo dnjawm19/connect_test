092c25145d4a9d7e459da2e18205a42987f2b668
jeongsoo <dnjawm19@naver.com>
2022-10-26 20:27:45 +0900

    테스트

--- src/test/java/com/week06/team01_week06_project/Team01Week06ProjectApplicationTests.java
+++ src/test/java/com/week06/team01_week06_project/Team01Week06ProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.week06.team01_week06_project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Team01Week06ProjectApplicationTests {
 
     @Test
